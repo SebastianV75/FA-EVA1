4daa302b5faed5ca223f421827bbd2bc27778cb7
SebastianV75 <92833858+SebastianV75@users.noreply.github.com>
2024-08-26 17:40:11 -0600

    Add files via upload

---
+++ EVA1_variables1/EVA_2_variables/src/eva_2_variables/EVA_2_variables.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva_2_variables;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA_2_variables {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {//Tipos de datos
+        System.out.println("Hola mundooo"); //Cadena de texto
+        System.out.println(100);            //Entero
+        System.out.println(3.1416);         //Numero con punto flotante
+        System.out.println('c');            //caracter
+        System.out.println(true);           //valores logicos (booleanos)
+        System.out.println(false);          //valores logicos (booleanos)
+                                            //; FIN de instruccion
+        
+        //ENTERO
+        //int
+        //long
+        //short
+        
+        //NUMERO CON PARTE DECIMAL
+        //float
+        //double
+        
+        //CADENAS DE TEXTO
+        //String
+        
+        //CARACTERES
+        //char
+        
+        //VALORES LOGICOS
+        //boolean
+        
+        //DECLARACION DE VARIABLES
+        //SINTAXIS PARA DECLARAR VARIABLES (REGLAS)
+        //TIPO_DE_DATO_NOMBRE_DE_LA_VARIABLE;
+        
+        String nombre;
+        String apellido;
+        String curp;
+        float promedio;
+        
+    }
+    
+}
